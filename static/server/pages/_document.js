b7391096c326eb36fb8fa5e34bbfd829ea461a10
Oscar Macieira <macieiraoscar@icloud.com>
2023-04-17 12:35:31 +0200

    fix(static): added static files
    Required for building with pre-prod packages

---
+++ static/server/pages/_document.js
@@ -0,0 +1,206 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 660;
+exports.ids = [660];
+exports.modules = {
+
+/***/ 4088:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ UltraDocument)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: ./node_modules/next/document.js
+var next_document = __webpack_require__(6859);
+;// CONCATENATED MODULE: external "@emotion/server/create-instance"
+const create_instance_namespaceObject = require("@emotion/server/create-instance");
+var create_instance_default = /*#__PURE__*/__webpack_require__.n(create_instance_namespaceObject);
+// EXTERNAL MODULE: ./src/utilities/createEmotionCache.ts + 1 modules
+var createEmotionCache = __webpack_require__(3540);
+;// CONCATENATED MODULE: ./src/pages/_document.tsx
+
+
+
+
+
+class UltraDocument extends next_document["default"] {
+    render() {
+        return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(next_document.Html, {
+            lang: "en",
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx(next_document.Head, {
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                        rel: "stylesheet",
+                        href: "https://fonts.googleapis.com/css?family=Inter:300,400,500,700&display=swap"
+                    })
+                }),
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("body", {
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx(next_document.Main, {}),
+                        /*#__PURE__*/ jsx_runtime_.jsx(next_document.NextScript, {})
+                    ]
+                })
+            ]
+        });
+    }
+}
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+UltraDocument.getInitialProps = async (ctx)=>{
+    // Resolution order
+    //
+    // On the server:
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. document.getInitialProps
+    // 4. app.render
+    // 5. page.render
+    // 6. document.render
+    //
+    // On the server with error:
+    // 1. document.getInitialProps
+    // 2. app.render
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. app.render
+    // 4. page.render
+    const originalRenderPage = ctx.renderPage;
+    // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+    // However, be aware that it can have global side effects.
+    const cache = (0,createEmotionCache/* default */.Z)();
+    const { extractCriticalToChunks  } = create_instance_default()(cache);
+    /* eslint-disable */ ctx.renderPage = ()=>originalRenderPage({
+            enhanceApp: (App)=>(props)=>/*#__PURE__*/ jsx_runtime_.jsx(App, {
+                        emotionCache: cache,
+                        ...props
+                    })
+        });
+    /* eslint-enable */ const initialProps = await next_document["default"].getInitialProps(ctx);
+    // This is important. It prevents emotion to render invalid HTML.
+    // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style)=>/*#__PURE__*/ jsx_runtime_.jsx("style", {
+            "data-emotion": `${style.key} ${style.ids.join(" ")}`,
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML: {
+                __html: style.css
+            }
+        }, style.key));
+    return Object.assign(Object.assign({}, initialProps), {
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: [
+            ...external_react_.Children.toArray(initialProps.styles),
+            ...emotionStyleTags
+        ]
+    });
+};
+
+
+/***/ }),
+
+/***/ 3540:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ utilities_createEmotionCache)
+});
+
+;// CONCATENATED MODULE: external "@emotion/cache"
+const cache_namespaceObject = require("@emotion/cache");
+var cache_default = /*#__PURE__*/__webpack_require__.n(cache_namespaceObject);
+;// CONCATENATED MODULE: ./src/utilities/createEmotionCache.ts
+
+const createEmotionCache = ()=>{
+    return cache_default()({
+        key: "css",
+        prepend: true
+    });
+};
+/* harmony default export */ const utilities_createEmotionCache = (createEmotionCache);
+
+
+/***/ }),
+
+/***/ 4140:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/get-page-files.js");
+
+/***/ }),
+
+/***/ 9716:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/htmlescape.js");
+
+/***/ }),
+
+/***/ 6368:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/utils.js");
+
+/***/ }),
+
+/***/ 6724:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/constants.js");
+
+/***/ }),
+
+/***/ 8743:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/html-context.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [859], () => (__webpack_exec__(4088)));
+module.exports = __webpack_exports__;
+
+})();
